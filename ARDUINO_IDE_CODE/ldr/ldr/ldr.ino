0e4cf1c196b519906fa829dc7bbb1c184afaf1ba
Ahmed-hesham001 <ahmed.hesham.amer2002@gmail.com>
2024-09-08 23:11:24 +0300

    added ldr and its simulation

---
+++ ARDUINO_IDE_CODE/ldr/ldr/ldr.ino
@@ -0,0 +1,23 @@
+#include <Atmega8_IO.h>
+
+#define ldr PIN_PC3
+#define led PIN_PB2
+
+int input = 0;
+int output = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ldr, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("hello");
+  input = analogRead(ldr);
+  Serial.println(input);
+  delay(1000);
+  output = map(input, 0, 1023, 0, 255);
+  analogWrite(led, output);
+}
